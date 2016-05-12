e32db749026ed3ed579e8b1af04ebe79a30648cb
cmgraff <cgraff1@gcc75.insa-rouen.fr>
2016-05-12 12:58:40 +0200

    A temporarily useful post installation script which can be copied into the target's root.

---
+++ toolchain/postinst.sh
@@ -0,0 +1,27 @@
+
+
+
+mkdir -p /newtoolchain/
+
+mount /dev/sdc /newtoolchain/
+
+mkdir -p /newtoolchain/src/
+
+mount /dev/sdb /newtoolchain/src/
+
+
+cp -r /toolchain/* /newtoolchain/
+
+chown -R liveuser /newtoolchain
+
+chmod 777 /tmp
+
+su - liveuser
+
+cd /newtoolchain/
+
+export PATH=/usr/local/bin:/usr/bin:/bin/:sbin
+
+export CC=gcc
+
+make > logfile 2>&1 &
