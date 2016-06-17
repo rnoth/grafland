0e982139b93257e783e3bc973c16bd573daac86b
cmgraff <cm0graff@gmail.com>
2016-06-17 09:48:59 +0200

    Improve the old 'cutils' error function.

--- cbin/lib/cutils.c
+++ cbin/lib/cutils.c
@@ -1,16 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h> 
 
-/* error function */
-void cutilerror(char *, int);
 
 void cutilerror(char *message, int i)
 {
-        fprintf(stderr, "%s", message);
-        if ( i > 0 )
-                perror("Error: ");
+	if ( i > 0 )
+		perror("Error: ");
+	fprintf(stderr, "%s", message);
 	if ( i > -1 )
-        	exit (i);
+		exit (i);
 }
-/* end error function */ 
+
