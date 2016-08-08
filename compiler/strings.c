78552e89e21c526fcd39f43e7c6f57cd67826705
cmgraff <cm0graff@gmail.com>
2016-08-07 19:13:02 -0500

    improve help report

--- compiler/strings.c
+++ compiler/strings.c
@@ -8,15 +8,16 @@
 	strings - find printable strings in files
 */
 
-size_t strings(char *);
+size_t strings(char *, size_t, char *);
 
 int main(int argc, char *argv[])
 { 
 	int o = 0;
 	size_t number = 0;
 	char *format = NULL;
+	char *posixspam = " [-a] [-t format] [-n number] [file...]\n";
 
-        while ((o = getopt (argc, argv, "at:n:")) != -1)
+        while ((o = getopt (argc, argv, "at:n:h")) != -1)
                 switch (o) {
                         case 'a':
 				break;
@@ -25,7 +26,11 @@ int main(int argc, char *argv[])
 			case 'n':
                        		break;
 			case 'h':
-				printf("strings [-a] [-t format] [-n number] [file...]");
+				if ( *argv )
+					write(2, *argv, strlen(*argv));
+				else
+					write(2, "strings", 7);
+				write(2, posixspam, strlen(posixspam));
                         default:
                                 break;
                 }
@@ -34,14 +39,14 @@ int main(int argc, char *argv[])
         argc -= optind;
 
 	if ( argc == 0 )
-		strings(0); /* STDIN_FILENO */
+		strings(0, number, format); /* STDIN_FILENO */
 
 	while ( *(argv) )
-		strings(*argv++);
+		strings(*argv++, number, format);
 
 	return 0; 
 }
-size_t strings(char *fd) 
+size_t strings(char *fd, size_t number, char *format)
 {
 	return 0; /* Change this to something useful */
 }
