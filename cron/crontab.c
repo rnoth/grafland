b86e3a6dca250c2f899c863a1f1252a787ceddcf
cmgraff <cm0graff@gmail.com>
2016-08-11 10:22:06 -0500

    function prototype for crontab()

--- cron/crontab.c
+++ cron/crontab.c
@@ -17,20 +17,27 @@
 		All POSIX comments are quoted, the author's are not
 */
 
+void crontab(char *, int []);
+
 int main(int argc, char *argv[])
 { 
 	int o = 0;
 	int ret = EXIT_SUCCESS; 
 	char *help = " [-e|-l|-r]\n";
+	/*  options .. e  l  r */
+	int opt[3] = { 0, 0, 0 };
 
 
 	while ((o = getopt (argc, argv, "elrh")) != -1)
 		switch (o) {
 			case 'e':
+				opt[0] = 1;
 				break;
 			case 'l':
+				opt[1] = 1;
 				break;
 			case 'r':
+				opt[2] = 1;
 				break;
 			case 'h':
                                 if ( *argv )
@@ -44,11 +51,18 @@ int main(int argc, char *argv[])
 	argv += optind;
 	argc -= optind;
 
+	
 	//if ( argc == 0 )
 	//	;
 
-	//while ( *(argv) )
-	//	*argv++;
-
+	while ( *(argv) )
+		crontab(*argv++, opt);
+	
 	return ret; 
+}
+
+void crontab(char *file, int opt[])
+{
+	
+	
 }
