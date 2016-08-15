60ac9a06b52b0f82d06ca20437dfd84c77dd9f70
cmgraff <cm0graff@gmail.com>
2016-08-16 00:30:31 +0200

    use uniform whitespace style

--- docs/C/simple_execute.c
+++ docs/C/simple_execute.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 	/* usage: ./simple_execute ls -la */
-	if ( argc > 1 )
+	if (argc > 1)
 	{
 		++argv;
 		execvp(*argv, argv);
 	}
 	return 0;
 }
+
