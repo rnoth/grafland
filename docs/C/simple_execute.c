22e64a879449f32843b4049bbd8bc253c6d36bf2
cmgraff <cm0graff@gmail.com>
2016-08-16 03:08:31 +0200

    use char *[]

--- docs/C/simple_execute.c
+++ docs/C/simple_execute.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	/* Usage: ./simple_execute ls -la */
 	if (argc > 1) 
-		execvp(*(argv + 1), (argv + 1));
+		execvp(argv[1], argv + 1);
 	
 	return 0;
 }
