8dfd8036295109db897547c996d9a3e7abe0aba8
cmgraff <cm0graff@gmail.com>
2016-08-16 02:43:39 +0200

    use a pointer offset

--- docs/C/simple_execute.c
+++ docs/C/simple_execute.c
@@ -3,11 +3,9 @@
 int main(int argc, char **argv)
 {
 	/* Usage: ./simple_execute ls -la */
-	if (argc > 1)
-	{
-		++argv;
-		execvp(*argv, argv);
-	}
+	if (argc > 1) 
+		execvp(*(argv + 1), (argv + 1));
+	
 	return 0;
 }
 
