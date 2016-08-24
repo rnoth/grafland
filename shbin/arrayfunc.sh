ef53c31bec1daab6418d1c6382a4b52794e0b7ac
Chris Graff <cgraff1@shaula.csit.parkland.edu>
2016-08-24 07:11:20 -0500

    utilize

---
+++ shbin/arrayfunc.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Implementation of a whitespace padded array in POSIX sh
+
+arrayd()
+{
+	INDICE="$1"
+	if [ $# -gt 1 ]
+	then 	shift
+	fi
+	printf "%s" $( eval printf '$'$INDICE) 
+}
+
+
+arrayd "$@"
+
+
