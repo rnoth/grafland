2f069941ad27ffd4c2e285c6c1c2388e8271f934
cmgraff <cm0graff@gmail.com>
2016-09-01 05:02:55 +0200

    bug in columnar list

--- shbin/ls.sh
+++ shbin/ls.sh
@@ -1,169 +1,140 @@
 #!/bin/sh
 
+LINES=$(tput lines)
+ROWS=$(tput cols)
+WHOAMI=$(whoami)
 
 shellstat()
-{
-
+{ 
 	FILE="$1"
-
-	WHOAMI=$(whoami)
-
 	printf "..."
 	if [ -r "$FILE" ]
-	then printf "r"
-	else printf "-"
+	then	printf "r"
+	else	printf "-"
 	fi 
-
 	if [ -w "$FILE" ]
-	then printf "w"
-	else printf "-"
+	then	printf "w"
+	else	printf "-"
 	fi 
-
 	if [ -x "$FILE" ]
-	then printf "x"
-	else printf "-"
+	then	printf "x"
+	else	printf "-"
 	fi
-
 	printf "..."
-
 	if [ -G "$FILE" ]
-	then printf " $WHOAMI :"
-	else printf " ....... :"
+	then	printf " $WHOAMI :"
+	else	printf " ....... :"
 	fi
-
 	if [ -O "$FILE" ]
-	then printf " $WHOAMI "
-	else printf " ....... "
+	then	printf " $WHOAMI "
+	else	printf " ....... "
 	fi
 	printf " $1\n"
 
 }
 
+# Argument parsing
 LONGLIST="0"
 ARGSTRING=""
 DEEP="0"
+HIDDEN="0"
 
 for i in $@
 do	case "$i" in
-	-l) LONGLIST="1"
-	shift
-	;;
-	-R) DEEP="1"
-	shift
-	;;
-	*) ARGSTRING="${ARGSTRING} ${i}"
-	;;
+	-l)	LONGLIST="1"
+		shift
+		;;
+	-R)	DEEP="1"
+		shift
+		;;
+	-a)	HIDDEN="1"
+		shift
+		;;
+	*)	ARGSTRING="${ARGSTRING} ${i}"
+		;;
 	esac
 done
 
 
-LINES=$(tput lines)
-ROWS=$(tput cols)
-
+# Default to printing the current working directory
 WORKD="."
 
 if [ "$#" -gt "0" ]
 then	WORKD="$ARGSTRING"
 fi
 
-
-
+HSTARS=""
 STARS="${WORKD}/*"
-
-if [ "$DEEP" = "1" ]
-then	STARS="${WORKD}/* ${WORKD}/*/* ${WORKD}/*/*/* ${WORKD}/*/*/*/* "
+if [ "$HIDDEN" = "0" ]
+then	STARS="${WORKD}/*"
+else	HSTARS="${WORKD}/.*"
 fi
 
-CYCLE="0" 
-LINED="" 
-LILEN="0" 
-C="0" 
-N="0" 
+if [ "$DEEP" = "1" ]
+then	if [ "$HIDDEN" = "0" ]
+	then	STARS="${WORKD}/* ${WORKD}/*/* ${WORKD}/*/*/* ${WORKD}/*/*/*/* "
+	else	HSTARS="${WORKD}/.* ${WORKD}/.*/.* ${WORKD}/.*/.*/.* ${WORKD}/.*/.*/.*/.* "
+	fi
+fi
+
+N="1" 
 LEN="0" 
 TLEN="0"
 
-for i in $STARS 
+for i in $STARS $HSTARS
 do	TLEN="${#i}"
 	[ -e "$i" ] || continue
 	if [ "$TLEN" -gt "$LEN" ]
 	then	LEN="${TLEN}"
 	fi
-	eval ARRAY_"$C"="$i"
+	eval ARRAY_"$N"="$i"
+	eval OARRAY_"$N"="......"
 
 	N=$(( $N + 1))
-	C=$(( $C + 1 ))
 done 
 
-# sorting
-#C="1"
-#while [ "$C" -lt "$N" ]
-#do D="$C" 
-#while [ "$D" -gt "0" -a \
-#"$( eval printf '$'ARRAY_$D )" -lt \
-#"$( eval printf '$'ARRAY_$(( $D - 1 )) )" ]
-#do 
-#HOLD="$( eval printf '$'ARRAY_$D )" 
-#eval ARRAY_"$D"="$( eval printf '$'ARRAY_$(( $D - 1 )) )" 
-#eval ARRAY_$(( $D - 1 ))="$HOLD"
-#D=$(( $D - 1 ))
-#done 
-#C=$(( $C + 1 ))
-#done 
   
 # screen width calculations
-C="1"
-if [ $LEN -lt $ROWS ]
-then	NUMIT=$(($ROWS / $LEN))
-else	NUMIT="1"
-fi
+C="0"
+NUMIT=$(($ROWS / $LEN))
+#if [ "$LEN" -lt "$ROWS" ]
+#then	
+#else	NUMIT="1"
+#fi
 
 
-cnt="0"
-sft="0"
-sftc="0"
-vary="0"
+cnt="1"
+sft="1"
+sftc="1"
+vary="1"
 
 # shuffle
-#while [ "$cnt" -lt "$N" ]
-#do 
-#	cnt=$(( $cnt + 1 )) 
-#done
+while [ "$cnt" -lt "$N" ]
+do	eval OARRAY_"$sft"="$( eval printf '$'ARRAY_$cnt )"
+	#eval OARRAY_"$cnt"="$( eval printf '$'ARRAY_$cnt )"
+	sft=$(($sft + $NUMIT ))
+	sftc=$(($sftc + 1))
+	if [ $sftc = $(($(( $N / $NUMIT)) + 1)) ]
+	then	vary=$(( $vary + 1 ))
+		sft=$vary
+		
+		sftc="0"
+	fi
+	
+	cnt=$(( $cnt + 1 )) 
+done
 
 # print
+C="1"
 while [ "$C" -lt "$N" ]
 do	if [ $LONGLIST -ne "0" ]
-	then	shellstat "$( eval printf '$'ARRAY_$C )"
-	else	printf "%-*s " "$LEN" "$( eval printf '$'ARRAY_$C )"
-	
-		if [ $(( $C % $NUMIT )) = "0" ]
+	then	shellstat "$( eval printf '$'OARRAY_$C )"
+	else	printf "%-*s " "$LEN" "$( eval printf '$'OARRAY_$C )"
+		if [ $(($C % $NUMIT)) = "0" ]
 		then	printf "\n"
 		fi
 	fi
 	C=$(( $C + 1 )) 
 done
 printf "\n"
-
-
-
-#void shift_alpha(int c, int refactor)
-#{
-#	/* Format columnar lists to alphabetize vertically */
-#	int cnt, sft, sftc, vary = 0;
-#	cnt = sft = sftc = vary = 0;
-#	while ( cnt < c )
-#	{ 
-#		global.output[sft] = global.strings[cnt];
-#		sft = (sft + refactor);
-#		++sftc;
-#		if (sftc == ( c / refactor ) + 1)
-#		{
-#			++vary;
-#			sft = vary;
-#			sftc = 0;
-#		}
-#		++cnt;
-#	} 
-#}
-
-
 
