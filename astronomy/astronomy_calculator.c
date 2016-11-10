b4f53bd67791b3d3b30d7ee2387a23834699ae17
cmgraff <cm0graff@gmail.com>
2016-11-10 16:44:45 +0100

    some stubs for stuff I am studying, nothing of interest here

---
+++ astronomy/astronomy_calculator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+        double M = 0;
+        double m = 0;
+        double extinction = 0;
+        double parsecs = 0;
+        double extinction_divisor = 1000;
+        double real_M = 0;
+        double m_minus_M = 0;
+
+        /* (m-M) = distance v*/
+
+        /* log10^d = [(mv - Mv) + 5] / 5 */
+
+        real_M = (M / exitinction_divisor);
+        m_minus_M = ( m - real_M);
+
+
+        return 0;
+}
