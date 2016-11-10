6ff384748005d519810f6821f3f294154b1f3fb8
cmgraff <cm0graff@gmail.com>
2016-11-10 16:57:40 +0100

    still bulking out variables

--- astronomy/astronomy_calculator.c
+++ astronomy/astronomy_calculator.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char *argv[])
 {
@@ -9,16 +11,32 @@ int main(int argc, char *argv[])
         double extinction_divisor = 1000;
         double real_M = 0;
         double m_minus_M = 0;
+	double myvar = 0;
 
         /* (m-M) = distance v*/
 
         /* log10^d = [(mv - Mv) + 5] / 5 */
 
-        real_M = (M / exitinction_divisor);
+        real_M = (M / extinction_divisor);
         m_minus_M = ( m - real_M);
 
 
+
+
+       
+      
+     
+    
+   
+
+        myvar = strtod(argv[1], 0);
+        //arg2 = atoi(argv[2]);
 	
 	/* log(pow(10, d)) = [(mv - Mv) + 5] / 5 */
+
+	//if ( argc > 0 )
+	
+
+	printf("%lf\n", log(pow(10, myvar)));
         return 0;
 }
