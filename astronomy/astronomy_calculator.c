9233a7d58d1e8b9e76c27f6d6349bcc4a24ccd4a
cmgraff <cm0graff@gmail.com>
2016-11-13 12:59:47 +0100

    extinction bulked out

--- astronomy/astronomy_calculator.c
+++ astronomy/astronomy_calculator.c
@@ -6,15 +6,8 @@
 /* log10(d) = (((m - M) + 5) / 5) */
 
 
-	// polaris 1.98 -3.6 133
-	
-
-
-
-	// sun
-	// m = -26.74
-	// M = 4.83
-	// parsecs = 0.000004848
+// polaris 1.98 -3.6 133 
+// sun -26.74 4.83 0.000004848
 
 int main(int argc, char *argv[])
 {
@@ -24,15 +17,7 @@ int main(int argc, char *argv[])
         double extinction_divisor = 1000;
 	double extinction = 0;
         double real_M = 0;
-        double m_minus_M = 0;
-
-
-
-	
-        
-
-       
-      
+        double m_minus_M = 0; 
      
 	if (argc < 4 )
 	{
@@ -44,23 +29,20 @@ int main(int argc, char *argv[])
         m = strtod(argv[1], 0);
         M = strtod(argv[2], 0);
 	parsecs = strtod(argv[3], 0);
-	
-	/* log(pow(10, d)) = [(mv - Mv) + 5] / 5 */
-	/* log(pow(10, d)) = ((m - M) + 5] / 5) */ 
+
 	
 	
 	printf("distance == %lf\n", log10(parsecs));
-	printf("(m-M) == %lf\n", (((m - M) + 5) / 5));
-	
+	printf("(m-M) == %lf\n", (((m - M) + 5) / 5)); 
 	
 	printf("After an extinction factor of %lf:\n", extinction_divisor);
 	
 	parsecs = log10(parsecs);
 	m_minus_M = (((m - M) + 5) / 5);
-
-        real_M = (M / extinction_divisor);
-        m_minus_M = ( m - real_M); 
-	printf("distance == %lf\n", parsecs);
+	extinction = (( parsecs / extinction_divisor) * 1.8);
+      
+       
+	printf("distance == %lf\n", parsecs - extinction);
         printf("(m-M) == %lf\n", m_minus_M);
 
 
