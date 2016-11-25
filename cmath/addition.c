ebb88000db86043aadfe80c9c6ec2af155c0d176
cmgraff <cm0graff@gmail.com>
2016-11-25 15:15:12 +0100

    try starting iterators at 1

--- cmath/addition.c
+++ cmath/addition.c
@@ -10,12 +10,18 @@ double addition(double x, double y)
 double multiplication(double x, double y)
 {
 	size_t bulk = y;
-	size_t i = 0;
+	double hold = x;
+	size_t i = 1;
+	
 	while ( i < y )
 	{
-		x+=y;
 		++i;
+		
+		x+=hold;
+		
+		
 	}
+
 	return x;
 	
 }
