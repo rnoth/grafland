f28b6fd6151de87ad7a51dda3eb50b4bb3f30928
cmgraff <cm0graff@gmail.com>
2016-12-21 17:55:43 -0600

    complete overhaul of the shell bin

--- shbin/remove_executable.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for i in * */* */*/* */*/*/*
-do	if [ -d "$i" ]
-	then echo "a dir"
-	else chmod -x "$i"
-	fi
-done
-
--- shbin/utf-8.sh
+++
@@ -1,5 +0,0 @@
-i=0
-while [ "$i" -lt 207  ]
-do 	i=$((i + 1))
-	printf "\342\\${i}\240"
-done
