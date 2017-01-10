cfa138c9ee129d943564238c15f587765295005c
cmgraff <cm0graff@gmail.com>
2017-01-09 19:02:35 -0600

    allocation wrapper libraries

--- lib/cutils.h
+++ lib/cutils.h
@@ -1,5 +1,3 @@
-
-
 void cutilerror(char *message, int i)
 {
 	if ( i > 0 )
@@ -8,4 +6,29 @@ void cutilerror(char *message, int i)
 	if ( i > -1 )
 		exit (i);
 }
+
+void * cutilmalloc(size_t len)
+{
+	void *ret;
+	if (!(ret = realloc(len))
+		cutilerror("cutilmalloc failed\n", 1);
+	return ret;
+}
+
+static void *cutilcalloc(size_t nmemb, size_t size)
+{
+        void *ret;
+        if(!(ret = calloc(nmemb, size))
+                cutilerror("cutilcalloc failed\n", 1);
+        return ret;
+}
+
+static void *cutilrealloc(void *ptr, size_t size)
+{
+        void *ret;
+        if(!(ret = realloc(ptr, size)))
+        	cutilerror("cutilrealloc failed\n", 1); 
+        return ret;
+}
+
 
