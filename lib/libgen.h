4567c04e5b1e50c4c8527d05415bc25f77697047
cmgraff <cm0graff@gmail.com>
2017-01-12 09:32:55 -0600

    libgen.h stub

---
+++ lib/libgen.h
@@ -0,0 +1,13 @@
+/* function prototypes */
+char *gbasename(char *);
+char *gdirname(char *);
+
+/* functions */
+char *gbasename(char *str) /* not yet implemented */
+{
+	ret str;
+}
+char *gdirname(char *str)  /* not yet implemented */
+{
+	ret str;
+}
