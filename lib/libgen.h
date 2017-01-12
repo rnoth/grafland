31e58c937dd82bfa6e5d589bd4c0f59a6cb25dd5
cmgraff <cm0graff@gmail.com>
2017-01-12 09:42:56 -0600

    gnu style basename

--- lib/libgen.h
+++ lib/libgen.h
@@ -1,13 +1,21 @@
 /* function prototypes */
 char *gbasename(char *);
 char *gdirname(char *);
+char *ggnu_basename(char *path);
 
 /* functions */
 char *gbasename(char *str) /* not yet implemented */
 {
 	ret str;
 }
+
 char *gdirname(char *str)  /* not yet implemented */
 {
 	ret str;
+}
+
+char *ggnu_basename(char *path) /* non-POSIX GNU version of basename */
+{
+	char *base = strrchr(path, '/');
+	return base ? base+1 : path;
 }
