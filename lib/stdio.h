feed95c225b9a49468d2eeaab406d5b449aee258
cmgraff <cm0graff@gmail.com>
2017-01-14 15:58:22 -0600

    stdio stream buffering stubs put into stdio.h

--- lib/stdio.h
+++ lib/stdio.h
@@ -1,18 +1,8 @@
-/*
-	Copyright 2017. CM Graff "stdio.h"
-	See LICENSE for copying details
-*/
-
-/* defines */
-/* ------- */
-#define GBUFSIZEE 4096
-#define IRCBUFSIZ GBUFSIZEE
-//#define HASLIBM
-
-/* self hosting dependencies */
-/* ------------------------- */
+/* libraries */
+/* --------- */ 
 #include <stddef.h>
 #include <stdarg.h>
+#include <fcntl.h>
 
 /* local libraries */
 /* --------------- */
@@ -20,12 +10,58 @@
 #ifdef HASLIBM
 #include "dtoa.h"
 #endif
-#include "kr2-stdio.h"
 
-/* function declarations */
-/* --------------------- */
-//int ggetchar(void);
-//int gputchar(char);
+/* defines */
+/* ------- */
+#define GNULL		0
+#define EOF		(-1)
+#define GBUFSIZ		1024
+#define OPEN_MAX	20	/* max #files open at once */ 
+#define gstdin		(&_iob[0])
+#define gstdout		(&_iob[1])
+#define gstderr		(&_iob[2]) 
+#define feof(p)		((p)->flag & _EOF) != 0)
+#define ferror(p)	((p)->flag & _ERR) != 0)
+#define fileno(p)	((p)->fd)
+#define ggetc(p)	(--(p)->cnt >= 0 ? (unsigned char) *(p)->ptr++ : _fillbuf(p))
+#define gputc(x,p) 	(--(p)->cnt >= 0 ? *(p)->ptr++ = (x) : _flushbuf((x),p))
+#define ggetchar()	ggetc(gstdin)
+#define gputchar(x)	gputc((x), gstdout)
+#define PERMS		0666	/* RW for owner, group, others */
+
+#define GBUFSIZEE 4096
+#define IRCBUFSIZ GBUFSIZEE
+
+typedef struct _iobuf {
+	int cnt;		/* characters left */
+	char *ptr;		/* next character position */
+	char *base;		/* location of buffer */
+	int flag;		/* mode of file access */
+	int fd;			/* file descriptor */
+} GFILE;
+
+//extern GFILE _iob[OPEN_MAX];
+
+enum _flags {
+	_READ	 =	01,	/* file open for reading  01 */	
+	_WRITE	=	01,	/* file open for writing  02 */
+	_UNBUF	=	01,	/* file is unbuffered  04 */
+	_EOF	=	01,	/* EOF has occurred on this file 010 */
+	_ERR	=	01	/* error occurred on this file 020 */
+};
+
+GFILE _iob[OPEN_MAX] = {	/* stdin, stdout, stderr */
+	{ 0, (char *) 0, (char *) 0, _READ, 0 },
+	{ 0, (char *) 0, (char *) 0, _WRITE, 1 },
+	{ 0, (char *) 0, (char *) 0, _WRITE | _UNBUF, 2 }
+};
+
+/* Function prototypes */ 
+/* ------------------- */ 
+int _fillbuf(GFILE *);
+int _flushbuf(int, GFILE *);
+int gggetchar(void);
+int ggputchar(char);
 size_t ggetline(char [], int);
 int gprintf_inter(int, char *, size_t, int, char *, va_list);
 int gprintf(char *, ...);
@@ -44,8 +80,7 @@ int gvfprintf(GFILE *, char *, va_list);
 /* --------- */
 
 /* single char io  */
-/*
-int ggetchar(void)
+int gggetchar(void)
 { 
 	static char buf[GBUFSIZEE];
 	static char *bufp = buf;
@@ -66,13 +101,13 @@ int ggetchar(void)
 	return -1;
 } 
 
-int gputchar(char c)
+int ggputchar(char c)
 { 
 	char buf[1];
 	buf[0] = c;
 	return write(1, buf, 1);
 }
-*/
+
 /* line retrieval */
 size_t ggetline(char s[], int lim)
 {
@@ -276,4 +311,149 @@ int gvfprintf(GFILE *stream, char *fmt, va_list argptr) /* not implemented */
 	va_end(argptr);
 	return ret;
 }
+
+/* fopen family (not implemented) */
+GFILE *gfopen(char *name, char *mode)
+{
+	int fd;
+	GFILE *fp;
+	if (*mode != 'r' && *mode != 'w' && *mode != 'a')
+		return GNULL;
+	for (fp = _iob; fp < _iob + OPEN_MAX; fp++)
+		if ((fp->flag & (_READ | _WRITE)) == 0)
+			break;		/* found free slot */
+	if (fp >= _iob + OPEN_MAX)	 /* no free slots */
+		return GNULL;
+	if (*mode == 'w')
+		fd = creat(name, PERMS);
+	else if (*mode == 'a') {
+		if ((fd = open(name, O_WRONLY, 0)) == -1)
+			fd = creat(name, PERMS);
+		lseek(fd, 0L, 2);
+	} else
+		fd = open(name, O_RDONLY, 0);
+	if (fd == -1)		 /* couldn't access name */
+		return GNULL;
+	fp->fd = fd;
+	fp->cnt = 0;
+	fp->base = GNULL;
+	fp->flag = (*mode == 'r') ? _READ : _WRITE;
+	return fp;
+}
+
+/* _fillbuf: allocate and fill input buffer */
+int _fillbuf(GFILE *fp)
+{
+	int bufsize;
+	if ((fp->flag&(_READ|_EOF|_ERR)) != _READ)
+		return EOF;
+	bufsize = (fp->flag & _UNBUF) ? 1 : GBUFSIZ;
+	if (fp->base == GNULL)		/* no buffer yet */
+		if ((fp->base = (char *) malloc(bufsize)) == GNULL)
+			 return EOF;		 /* can't get buffer */
+	fp->ptr = fp->base;
+	fp->cnt = read(fp->fd, fp->ptr, bufsize);
+	if (--fp->cnt < 0) {
+		if (fp->cnt == -1)
+			 fp->flag |= _EOF;
+		else
+			 fp->flag |= _ERR;
+		fp->cnt = 0;
+		return EOF;
+	}
+	return (unsigned char) *fp->ptr++;
+}
+
+/* _flushbuf */
+int _flushbuf(int c, GFILE *f)
+{
+	int num_written, bufsize;
+	unsigned char uc = c;	
+
+	if ((f->flag & (_WRITE|_EOF|_ERR)) != _WRITE)
+		return EOF;
+	if (f->base == GNULL && ((f->flag & _UNBUF) == 0)) {
+		/* no buffer yet */
+		if ((f->base = malloc(GBUFSIZ)) == GNULL) 
+			/* couldn't allocate a buffer, so try unbuffered */
+			f->flag |= _UNBUF;
+		else {
+			f->ptr = f->base;
+			f->cnt = GBUFSIZ - 1;
+		}
+	}
+	if (f->flag & _UNBUF) {
+		/* unbuffered write */
+		f->ptr = f->base = GNULL;
+		f->cnt = 0;
+		if (c == EOF)
+			return EOF;
+		num_written = write(f->fd, &uc, 1);
+		bufsize = 1;
+	} else {
+		/* buffered write */
+		if (c != EOF)
+	{
+			//f->ptr = uc;
+		f->ptr++;
+	}
+		bufsize = (int)(f->ptr - f->base);
+		num_written = write(f->fd, f->base, bufsize);
+		f->ptr = f->base;
+		f->cnt = GBUFSIZ - 1;
+	}
+	if (num_written == bufsize)
+		return c;
+	else {		 
+		f->flag |= _ERR;
+		return EOF;
+	} 
+}
+
+int gfflush(GFILE *f)
+{
+	int retval;
+	//int i;
+
+	retval = 0;
+	if (f == GNULL) {
+		/* flush all output streams */ 
+	/*
+		for (i = 0; i < OPEN_MAX; i++) {
+			if ((_iob[i]->flag & _WRITE) && (gfflush(EOF, _iob[i]) == -1))
+	
+				retval = -1;
+		}
+	*/
+	;
+	} else {
+		if ((f->flag & _WRITE) == 0)
+			return -1;
+		_flushbuf(EOF, f);
+		if (f->flag & _ERR)
+			retval = -1;
+	}
+	return retval;
+}
+
+int gfclose(GFILE *f)
+{
+	int fd;
+	
+	if (f == GNULL)
+		return -1;
+	fd = f->fd;
+	gfflush(f);
+	f->cnt = 0;
+	f->ptr = GNULL;
+	if (f->base != GNULL)
+		free(f->base);
+	f->base = GNULL;
+	f->flag = 0;
+	f->fd = -1;
+	return close(fd);
+}
+
+
+
 
