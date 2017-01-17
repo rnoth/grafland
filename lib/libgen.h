d4e4a02395b150a522edc7c400a9227e80da8abf
cmgraff <cm0graff@gmail.com>
2017-01-16 22:16:00 -0600

    fast basename

--- lib/libgen.h
+++ lib/libgen.h
@@ -9,18 +9,38 @@ char *gdirname(char *);
 char *ggnu_basename(char *path);
 
 /* functions */
-char *gbasename(char *str) /* not yet implemented */
-{
-	return str;
-}
-
 char *gdirname(char *str)  /* not yet implemented */
 {
 	return str;
 }
 
-char *ggnu_basename(char *path) /* non-POSIX GNU version of basename */
+char *gnu_basename(char *path) /* non-POSIX GNU version of basename */
 {
-	char *base = strrchr(path, '/');
+	char *base; 
+	base = strrchr(path, '/');
 	return base ? base+1 : path;
+}
+
+
+char *gbasename(char *path)
+{
+	size_t i = 0;
+	int pathtab[4096] = { 0 };
+	size_t npth = 0;
+
+	for( i = 0; path[i] != '\0' && i < 4096; i++)
+	{
+		if ( path[i] == '/' )
+			pathtab[npth++] = i;
+	}
+	for ( ; i > 1 && path[--i] == '/' ; )
+	{
+		path[i] = '\0';
+		--npth;
+	}
+	
+	if ( npth > 1)
+		return path + pathtab[npth - 1] + 1; 
+	else 
+		return path;
 }
