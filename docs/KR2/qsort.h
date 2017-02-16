b161896cc81203bc62215b4858fa2d82941a47ee
cmgraff <cm0graff@gmail.com>
2017-02-16 10:57:44 -0500

    remove all the old 2014 documentation for various OS

--- docs/KR2/qsort.h
+++ docs/KR2/qsort.h
@@ -1,26 +1,25 @@
-   /* qsort: sort v[left]...v[right] into increasing order */
-   void kr2qsort(int v[], int left, int right)
-   {
-        int i, last;
-        void swap(int v[], int i, int j);
-        if (left >= right) /* do nothing if array contains */
-              return;               /* fewer than two elements */
-        swap(v, left, (left + right)/2); /* move partition elem */
-        last = left;                                     /* to v[0] */
-        for (i = left + 1; i <= right; i++) /* partition */
-              if (v[i] < v[left])
-                   swap(v, ++last, i);
-        swap(v, left, last);                            /* restore partition elem */
-        qsort(v, left, last-1);
-        qsort(v, last+1, right);
-   }
-//We moved the swapping operation into a separate function swap because it occurs three times in qsort.
-   /* swap: interchange v[i] and v[j] */
-   void swap(int v[], int i, int j)
-   {
-        int temp;
-        temp = v[i];
-        v[i] = v[j];
-        v[j] = temp;
-   }
+/* qsort: sort v[left]...v[right] into increasing order */
+void qsort(void *v[], int left, int right,
+           int (*comp)(void *, void *))
+{
+    int i, last;
+    void swap(void *v[], int, int);
+    if (left >= right)    /* do nothing if array contains */
+        return;           /* fewer than two elements */
+    swap(v, left, (left + right)/2);
+    last = left;
+    for (i = left+1; i <= right; i++)
+        if ((*comp)(v[i], v[left]) < 0)
+            swap(v, ++last, i);
+    swap(v, left, last);
+    qsort(v, left, last-1, comp);
+    qsort(v, last+1, right, comp);
+}
+void swap(void *v[], int i, int j;)
+{
+  void *temp;
+  temp = v[i];
+  v[i] = v[j];
+  v[j] = temp;
+}
 
