91ca6d0e0ab72c09c6afd813babf992ba25a5196
Dayvi Schuster <dayvi1990@gmail.com>
2017-03-20 19:49:30 +0100

    Created: incomplete hashtable

---
+++ libc/src/search/hashtable.c
@@ -0,0 +1,60 @@
+#include "hashtable.h"
+
+htable_t *htable_create(int size){
+	htable_t *htable_v = NULL;
+	if(size <= 0)
+		return NULL;
+	if((htable_v = malloc(sizeof(htable_t)))== NULL)
+		return NULL;
+	if( ( htable_v->table = malloc( sizeof( kv_pair_t * ) * size ) ) == NULL ) 
+		return NULL;
+	for(int i = 0; i < size; i++){
+		htable_v->table[i] = NULL;
+	}
+	htable_v->size = size;
+	return htable_v;
+};
+
+
+int htable_hash(htable_t *htable_v, char *key){
+	unsigned long int hval;
+	int i = 0;
+	while(hval < ULONG_MAX && i < strlen(key)){
+		hval = hval << 8;
+		hval += key[i];
+		i++;
+	}
+	return hval % htable_v->size;
+};
+
+kv_pair_t *htable_newpair(char *key, char *value){
+	kv_pair_t *newpair = NULL;
+	if((newpair = malloc(sizeof(kv_pair_t)))==NULL)
+		return NULL;
+	if((newpair->key = strdup(key))==NULL)
+		return NULL;
+	if((newpair->value = strdup(value))==NULL)
+		return NULL;
+	newpair->next = NULL;
+	return newpair;
+};
+
+void htable_set(htable_t *htable_v,char *key,char *value){
+	//TODO: finish up the htable_set method that actually allows us to populate the damn thing
+};
+char *htable_get(htable_t *htable_v,char *key){
+	int bin = 0;
+	kv_pair_t *pair;
+
+	bin = htable_hash(htable_v,key);
+
+	pair = htable_v->table[bin];
+	while(pair != NULL && pair->key != NULL && strcmp(key,pair->key) > 0){
+		pair = pair->next;
+	}
+	if(pair == NULL || pair->key == NULL || strcmp(key,pair->key) != 0)
+		return NULL;
+	else
+		return pair->value;
+};
+
