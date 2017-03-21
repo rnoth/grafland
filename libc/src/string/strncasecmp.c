7068508e0fa2b3a98de8f2134562a37e673069a5
Iain Hill <iainardernhill@gmail.com>
2017-03-21 08:11:18 -0500

    [libc] Added strncasecmp

---
+++ libc/src/string/strncasecmp.c
@@ -0,0 +1,26 @@
+#include <gstddef.h>
+#include <gctype.h>
+#include <gstring.h>
+
+int gstrncasecmp(const char *s, const char *t, size_t n)
+{
+	int s_bis, t_bis;
+	size_t i = 0;
+
+        while (*s && *t && i++ < n)
+	{
+		s_bis = tolower(*s);
+		t_bis = tolower(*t);
+
+		if (s_bis == t_bis) {
+			if (*s == '\0')
+				return 0;
+			s++;
+			t++;
+		}
+		else
+			break;
+	}
+        return s_bis - t_bis;
+}
+
