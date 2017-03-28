a64b3020d07a1ab8c45ba2e4766f71a4408a0998
cmgraff <cm0graff@gmail.com>
2017-03-27 20:17:01 -0500

    [libc] continue bulking out popen.

--- libc/src/stdio/popen.c
+++ libc/src/stdio/popen.c
@@ -3,7 +3,42 @@
 GFILE *gpopen(const char *command, const char *type)
 {
 	GFILE *ret;
-	/* needs to call fork and execvp */
+	int pipefd[2] = { 0 };
+        ssize_t len = 0;
+        size_t total = 0;
+        char *buffer;
+	size_t bufsize = 4096;
+	
+
+        if (!(buffer = malloc(bufsize)))
+                return 0;
+        pipe(pipefd);
+
+        if (fork() == 0)
+        {
+		const char *argv[4] = { "/bin/sh", "-c", NULL, NULL};
+		argv[2] = command;
+                close(pipefd[0]);
+                dup2(pipefd[1], 1);
+                close(pipefd[1]);
+                dup2(pipefd[1], 2);
+                execvp(argv[0], argv + 1);
+                _exit(1);
+        }
+        else
+        {
+                close(pipefd[1]);
+		/*
+                while ((len = read(pipefd[0], buffer + total, PIPE_BUF)))
+                {
+                        total +=len;
+                        if (!(buffer = realloc(buffer, total + PIPE_BUF + 1)))
+                                return 0;
+                } 
+                write(1, buffer, total);
+		*/
+        }
+	
 	return ret;
 }
 
