d6151ac81efc69629f4434e962f340a38efe2395
cmgraff <cm0graff@gmail.com>
2017-04-01 09:33:43 -0500

    [libc] bulk out a basic limits.h

---
+++ libc/include/glimits.h
@@ -0,0 +1,17 @@
+/* constants */
+#define CHAR_BITS    8
+
+/* Gnerated automatically by ./tests/compute_limits-driver: */
+#define SIZE_MIN     0
+#define SSIZE_MAX    9223372036854775808
+#define SSIZE_MIN    -9223372036854775808
+#define UCHAR_MAX    255
+#define UCHAR_MIN    0
+#define CHAR_MAX     127
+#define CHAR_MIN     -128
+#define INT_MAX      2147483647
+#define INT_MIN      -2147483648
+#define LONG_MAX     9223372036854775807
+#define LONG_MIN     -9223372036854775808
+#define LLONG_MAX    9223372036854775807
+#define LLONG_MIN    -9223372036854775808
