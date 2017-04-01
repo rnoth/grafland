8375d7ea45531652849fe019df70a1490e5cb435
evg <pndspirit@gmail.com>
2017-04-01 15:27:42 -0500

    [libc] Swab bulked out.

---
+++ libc/src/unistd/swab.c
@@ -0,0 +1,12 @@
+#include <unistd.h> // ?
+
+void swab(void *from, void *to, ssize_t n) {
+	char *fromm = from;
+	char *too = to;
+	for (int i = 0; i < n; i += 2) {
+		char tmp = *(fromm + i);
+		char tmpp = *(fromm + i + 1);
+		*(too + i) = tmpp;
+		*(too + i + 1) = tmp;
+	}
+}
