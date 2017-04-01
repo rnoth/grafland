afd8d6708083b47be63b970b24cd9d4217015371
cmgraff <cm0graff@gmail.com>
2017-03-31 19:41:33 -0500

    [libc] strerror working.

--- libc/src/errno/strerror.c
+++ libc/src/errno/strerror.c
@@ -1,7 +1,7 @@
 #include <gerrno.h>
 
-/* x86 ( incomplete )  */
-char *_messages[125] = {
+/* x86 */
+char *_messages[150] = {
 	"\0",					/* (not used)	0	*/
 	"Operation not permitted.\0",		/* EPERM	1	*/
 	"No such file or directory.\0",		/* ENOENT	2	*/
@@ -59,21 +59,21 @@ char *_messages[125] = {
 	"\0",					/* ?		54	*/
 	"\0",					/* ? 		55	*/
 	"Not a STREAM.\0",			/* ENOSTR	60	*/
-	"No msg is avail on STREAM head.",	/* ENODATA	61	*/
-	"Stream ioctl() timeout.",		/* ETIME	62	*/
-	"No STREAM resources.",			/* ENOSR	63	*/
+	"No msg is avail on STREAM head.\0",	/* ENODATA	61	*/
+	"Stream ioctl() timeout.\0",		/* ETIME	62	*/
+	"No STREAM resources.\0",		/* ENOSR	63	*/
 	"\0",					/* ?		64	*/
 	"\0",					/* ?		65	*/
 	"\0",					/* ? 		66	*/
-	"Reserved.",				/* ENOLINK	67	*/
+	"Reserved.\0",				/* ENOLINK	67	*/
 	"\0",					/* ?		68	*/
 	"\0",					/* ? 		69	*/
 	"\0",					/* ?		70	*/
-	"Protocol error.",			/* EPROTO	71	*/
-	"Reserved.",				/* EMULTIHOP	72	*/
+	"Protocol error.\0",			/* EPROTO	71	*/
+	"Reserved.\0",				/* EMULTIHOP	72	*/
 	"\0",					/* ?		73	*/
-	"Bad message.",				/* EBADMSG	74	*/
-	"Val too large to be stored in data type.",/* EOVERFLOW	75	*/
+	"Bad message.\0",			/* EBADMSG	74	*/
+	"Val too large to be stored in data type.\0",/*EOVERFLOW	75	*/
 	"\0",					/* ?		76	*/
 	"\0",					/* ?		77	*/
 	"\0",					/* ? 		78	*/
@@ -82,22 +82,59 @@ char *_messages[125] = {
 	"\0",					/* ?		81	*/
 	"\0",					/* ?		82	*/
 	"\0",					/* ? 		83	*/
-	"Illegal byte sequence.",		/* EILSEQ	84	*/
+	"Illegal byte sequence.\0",		/* EILSEQ	84	*/
 	"\0",					/* ?		85	*/
 	"\0",					/* ?		86	*/
 	"\0",					/* ?		87	*/
-	"Not a socket.",			/* ENOTSOCK	88	*/
-	"Destination address required.",	/* EDESTADDRREQ	89	*/
-	"Message too large.",			/* EMSGSIZE	90	*/
-	"Protocol wrong type for socket.",	/* EPROTOTYPE	91	*/
-	"Protocol not available.",		/* ENOPROTOOPT	92	*/
-	"Protocol not supported.", 	/* EPROTONOSUPPORT	93	*/
+	"Not a socket.\0",			/* ENOTSOCK	88	*/
+	"Destination address required.\0",	/* EDESTADDRREQ	89	*/
+	"Message too large.\0",			/* EMSGSIZE	90	*/
+	"Protocol wrong type for socket.\0",	/* EPROTOTYPE	91	*/
+	"Protocol not available.\0",		/* ENOPROTOOPT	92	*/
+	"Protocol not supported.\0", 	/* EPROTONOSUPPORT	93	*/
 	"\0",					/* ?		94	*/
-	"Operation not supported on socket.",	/* EOPNOTSUPP	95	*/
-	"Address family not supported.",	/* EAFNOSUPPORT	97	*/
-	"Address in use.",			/* EADDRINUSE	98	*/
-	"Address not available.",	/* EADDRNOTAVAIL	99	*/
-	"Network is down.",			/* ENETDOWN	100	*/
+	"Operation not supported on socket.\0",	/* EOPNOTSUPP	95	*/
+	"Address family not supported.\0",	/* EAFNOSUPPORT	97	*/
+	"Address in use.\0",			/* EADDRINUSE	98	*/
+	"Address not available.\0",	/* EADDRNOTAVAIL	99	*/
+	"Network is down.\0",			/* ENETDOWN	100	*/
+	"Network unreachable.\0",		/* ENETUNREACH	101	*/
+	"Connection aborted by network.\0",	/* ENETRESET	102	*/
+	"Connection aborted.\0",		/* ECONNABORTED 103	*/
+	"Connection reset.\0",			/* ECONNRESET	104	*/
+	"No buffer space available.\0",		/* ENOBUFS	105	*/
+	"Socket is connected.\0",		/* EISCONN	106	*/
+	"The socket is not connected.\0",	/* ENOTCONN	107	*/
+	"\0",					/* ?		108	*/
+	"\0",					/* ?		109	*/
+	"Connection timed out.\0",		/* ETIMEDOUT	110	*/
+	"Connection refused.\0",		/* ECONNREFUSED	111	*/
+	"\0",					/* ?		112	*/
+	"Host is unreachable.\0",		/* EHOSTUNREACH	113	*/
+	"Connection already in progress.\0",	/* EALREADY	114	*/
+	"Operation in progress.\0",		/* EINPROGRESS	115	*/
+	"Reserved.\0",				/* ESTALE	116	*/
+	"\0",					/* ?		117	*/
+	"\0",					/* ?		118	*/
+	"\0",					/* ?		119	*/
+	"\0",					/* ?		120	*/
+	"\0",					/* ?		121	*/
+	"Reserved.\0",				/* EDQUOT	122	*/ 
+	"\0",					/* 			*/
+	"\0",					/* 			*/
+	"\0",					/* 	padding		*/
+	"\0",					/*			*/
+	"\0",					/* 			*/ 
+	"\0",					/* 			*/
+	"\0",					/* 			*/
+	"\0",					/* 			*/
+	"\0",					/* 			*/
+	"\0",					/* 			*/ 
+	"\0",					/* 			*/
+	"\0",					/* 			*/
+	"\0",					/* 			*/
+	"\0",					/* 			*/
+	"\0",					/* 			*/
 	
 };
 
