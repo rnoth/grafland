8c1e6ae2eeec9305e9b27b4b562171ff5184de39
cmgraff <cm0graff@gmail.com>
2017-04-05 03:09:19 -0500

    [libc] use native g-functions.

--- libc/src/search/hashtable.c
+++ libc/src/search/hashtable.c
@@ -1,13 +1,14 @@
 #include "hashtable.h"
 
-htable_t *htable_create(int size){
-	int i = 0;
+htable_t *htable_create(size_t size)
+{
+	size_t i = 0;
 	htable_t *htable_v = NULL;
 	if(size <= 0)
 		return NULL;
-	if((htable_v = malloc(sizeof(htable_t)))== NULL)
+	if((htable_v = gmalloc(sizeof(htable_t))) == NULL)
 		return NULL;
-	if( ( htable_v->table = malloc( sizeof( kv_pair_t * ) * size ) ) == NULL ) 
+	if((htable_v->table = gmalloc(sizeof(kv_pair_t *) * size)) == NULL) 
 		return NULL;
 	for(i = 0; i < size; i++){
 		htable_v->table[i] = NULL;
@@ -16,49 +17,54 @@ htable_t *htable_create(int size){
 	return htable_v;
 }
 
-
-int htable_hash(htable_t *htable_v, char *key){
-	unsigned long int hval;
+int htable_hash(htable_t *htable_v, char *key)
+{ 
+	size_t hval = 0; // TODO: add ULONG_MAX to glimits.h, use size_t for now
 	size_t i = 0;
-	size_t len = strlen(key);
-	while(hval < ULONG_MAX && i < len){
+	size_t len = gstrlen(key);
+
+	for(i = 0; hval < GSIZE_MAX && i < len; ++i)
+	{
 		hval = hval << 8;
 		hval += key[i];
-		i++;
 	}
 	return hval % htable_v->size;
 }
 
-kv_pair_t *htable_newpair(char *key, char *value){
+kv_pair_t *htable_newpair(char *key, char *value)
+{
 	kv_pair_t *newpair = NULL;
-	if((newpair = malloc(sizeof(kv_pair_t)))==NULL)
+	if((newpair = gmalloc(sizeof(kv_pair_t)))==NULL)
 		return NULL;
-	if((newpair->key = strdup(key))==NULL)
+	if((newpair->key = gstrdup(key))==NULL)
 		return NULL;
-	if((newpair->value = strdup(value))==NULL)
+	if((newpair->value = gstrdup(value))==NULL)
 		return NULL;
 	newpair->next = NULL;
 	return newpair;
 }
 
-void htable_set(htable_t *htable_v,char *key,char *value){
-	value = value;
+void htable_set(htable_t *htable_v,char *key,char *value)
+{
+	value = value;				// suppress warnings
 	key = key;
 	htable_v = htable_v;
 	//TODO: finish up the htable_set method that actually allows us to populate the damn thing
 }
-char *htable_get(htable_t *htable_v,char *key){
-	htable_v = htable_v;
-	//int bin = 0;
+
+char *htable_get(htable_t *htable_v,char *key)
+{
+	htable_v = htable_v; 			// suppress warnings
+	//int bin = 0; 				// suppress warnings
 	kv_pair_t *pair;
 
-	//bin = htable_hash(htable_v,key);
+	//bin = htable_hash(htable_v,key);	// suppress warnings
 
-	//pair = htable_v->table[bin];
-	while(pair != NULL && pair->key != NULL && strcmp(key,pair->key) > 0){
+	//pair = htable_v->table[bin];		// suppress warnings
+	while(pair != NULL && pair->key != NULL && gstrcmp(key,pair->key) > 0){
 		pair = pair->next;
 	}
-	if(pair == NULL || pair->key == NULL || strcmp(key,pair->key) != 0)
+	if(pair == NULL || pair->key == NULL || gstrcmp(key,pair->key) != 0)
 		return NULL;
 	else
 		return pair->value;
--- libc/src/search/hashtable.h
+++ libc/src/search/hashtable.h
@@ -1,11 +1,13 @@
 #ifndef HASHTABLE_H_
 #define HASHTABLE_H_
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <limits.h>
-#include <string.h>
+/* internal headers */
+#include <gstdlib.h>
+#include <gstdio.h>
+#include <glimits.h>
+#include <gstring.h>
 
+/* structures and data types */
 struct kv_pair{
 	char *key;
 	char *value;
@@ -17,15 +19,14 @@ struct htable{
 	int size;
 	struct kv_pair_t **table;
 };
-typedef struct htable htable_t;
 
-htable_t *htable_create(int size);
+typedef struct htable htable_t; 
 
-int htable_hash(htable_t *htable_v, char *key);
-
-kv_pair_t *htable_newpair(char *key, char *value);
-
-void htable_set(htable_t *htable_v, char *key, char *value);
-char *ht_get(htable_t *htable_v, char *key);
+/* functions */
+htable_t *htable_create(size_t); 
+int htable_hash(htable_t *, char *); 
+kv_pair_t *htable_newpair(char *, char *); 
+void htable_set(htable_t *, char *, char *);
+char *ht_get(htable_t *, char *);
 
 #endif // HASHTABLE_H_
