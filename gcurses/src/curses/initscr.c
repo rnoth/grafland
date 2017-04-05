d6a4257c2b51f4cacea1bd017b7fd02101e55db2
cmgraff <cm0graff@gmail.com>
2017-04-05 13:00:35 -0500

    [curses] initialise stdscr.

--- gcurses/src/curses/initscr.c
+++ gcurses/src/curses/initscr.c
@@ -13,13 +13,20 @@ WINDOW *initscr(void)
 	if ((ret = ioctl(0, TIOCGWINSZ, &w)) == -1)
 		return NULL;
 
+	/* populate the global structure */
 	ansiglb.col = w.ws_col;
 	ansiglb.row = w.ws_row;
+
 	ansiglb.t_eraseall = T_ERASEALL;
 	ansiglb.t_insertreset = T_INSERTRESET;
 	ansiglb.t_gohome = T_GOHOME;
 	ansiglb.t_clrcur2bot = T_CLRCUR2BOT;
 	ansiglb.t_clrtoeol = T_CLRCUR2END;
+
+	/* populate stdscr */
+	win->x = ansiglb.col;
+	win->y = ansiglb.row;
+
 	len = sprintf(buf, "%s%s", ansiglb.t_eraseall, ansiglb.t_insertreset);
 	write(1, buf, len);
 	move(1, 1);
