66c9774e08d3b2e1430a9c685049fe38dff75b0a
cmgraff <cm0graff@gmail.com>
2017-04-06 18:12:05 -0500

    [libc] added index.c and rindex.c to libc/src/string/

---
+++ libc/src/string/index.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <strings.h>
+
+char *index (const char *s, int c) {
+	size_t i = 0;
+	while (s[i] != c && s[i] != '\0') {
+		++i;
+	}
+	if (s[i] != c && c != '\0')
+		return NULL;
+	return i + s;
+}
