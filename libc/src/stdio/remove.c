deb93503db0b92d52bbd9d6fa3001bb4a00fe5fc
cmgraff <cm0graff@gmail.com>
2017-04-10 18:30:25 -0500

    [libc] include unistd.h

--- libc/src/stdio/remove.c
+++ libc/src/stdio/remove.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
-#include <errno.h>
+#include <gstdio.h>
+#include <gerrno.h>
+#include <unistd.h>
 
-int gremove(const char *file) {
-	if (unlink(file)) {
+int gremove(const char *file)
+{
+	if (unlink(file))
+	{
 		if (errno == EISDIR)
 			return rmdir(file);
 		return -1;
 	}
-	return 0
+	return 0;
 }
