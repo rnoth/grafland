d995fb779c6afb4611855d7a73d07f727e28a412
moth <moth@airmail.cc>
2017-04-15 16:03:34 -0400

    re-work driver

--- new_arbprec/tests/arbprec-driver.c
+++ new_arbprec/tests/arbprec-driver.c
@@ -1,12 +1,62 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 #include <string.h>
 #include <arbprec/arbprec.h>
 
+size_t cardinal;         /* All array functions must have the same length (cardinality) */
+int base = 10;                   /* Default to base 10 */
 
-int base = 10; 		/* global base */
+int main(int argc, char **argv)
+{
+	int *a;
+	int *b;
+	int *c;
 
+	printf("allocating some big floats...");
+	fflush(stdout);
+
+	a = bigflt_alloc();
+	b = bigflt_alloc();
+	c = bigflt_alloc();
+
+	if (!a || !b) abort();
+
+	printf("done\n");
+
+	printf("testing addition..\n");
+
+	printf("\tpreforming some basic addition...");
+	fflush(stdout);
+	str2ints("3", a);
+	str2ints("4", b);
+	addition(a, b, c);
+	assert(isequal_to(c, 7));
+	printf("done\n");
+
+	printf("\tadding some large numbers...");
+	fflush(stdout);
+	str2ints("532153", a);
+	str2ints("3356544", b);
+	addition(a, b, c);
+	assert(isequal_to(c, 3888697));
+	printf("done\n");
+
+	printf("done (testing addition)\n");
+
+	printf("freeing the big floats...");
+	bigflt_free(a);
+	bigflt_free(b);
+	bigflt_free(c);
+	printf("done\n");
+	fflush(stdout);
+
+	return 0;
+}
+
+#if 0
 int main(int argc, char **argv)
 {
 	int *hold;	/* Copy of bigint1 */
@@ -18,14 +68,11 @@ int main(int argc, char **argv)
 	size_t cardb; 
 	int *bigint1;			/* Copy of argument 1 */
 	int *bigint2;			/* Copy of argument 2 */
-	size_t real_cardinality = 0;
-	size_t imag_cardinality = 0;
-	int *result;
 
-	while ((o = getopt (argc, argv, "vb:")) != -1)
+	while ((o = getopt(argc, argv, "vb:")) != -1)
 		switch (o) { 
 			case 'v':
-				verbosity(NULL, NULL, 1, 0);
+				verbosity(NULL, NULL, 1);
 				break;
 			case 'b': /* Override base */
 				base = strtoul(optarg, 0, 10);
@@ -54,10 +101,11 @@ int main(int argc, char **argv)
 	printf("/ %20lf\n", a / b);
 	
 
-	mirror = arbprec_malloc(1000 * sizeof(int));
-	result = arbprec_malloc(1000 * sizeof(int));
-	bigint1 = arbprec_malloc(1000 * sizeof(int));
-	bigint2 = arbprec_malloc(1000 * sizeof(int));
+	mirror = strallocate(1000 * sizeof(int));
+	result = strallocate(1000 * sizeof(int));
+	tmpmir = strallocate(1000 * sizeof(int));
+	bigint1 = strallocate(1000 * sizeof(int));
+	bigint2 = strallocate(1000 * sizeof(int));
 
 	/* arb divide */
 	printf("division\n");
@@ -65,58 +113,76 @@ int main(int argc, char **argv)
 	cardb = strlen(argv[1]);
 	hold = str2ints(argv[0], bigint1);
 	hold2 = str2ints(argv[1], bigint2);
-	hold[carda] = 4242;
-	hold2[cardb] = 4242;
-	result = divide(hold, hold2, result); 
+	hold[carda] = base;
+	hold2[cardb] = base;
+	divide(hold, hold2, result);
 
-	/* ... printing the float part of division is still a bit complicated */
-	if ( carda + 1 > cardb )
-                real_cardinality = carda - cardb + 1;
-        else
-                real_cardinality = carda; 
-        if ( carda >= real_cardinality )
-                imag_cardinality = carda - real_cardinality; 
-        printarray(result, real_cardinality); 
-        printarray(result + (real_cardinality) ,imag_cardinality);
-
-
-	/* multiply */
+	/* arb multiply */
 	printf("multiplication\n");
+
 	hold = str2ints(argv[0], bigint1);
 	hold2 = str2ints(argv[1], bigint2);
-	result = multiply(hold, hold2, result);
-	printarray(result, carda + cardb);
+	multiply(hold, hold2, result);
 
-	/* add */
+	/* arb addition */
 	printf("addition\n");
 	hold = str2ints(argv[0], bigint1);
 	hold2 = str2ints(argv[1], bigint2);
-	hold = addition(hold, hold2, result);
-	printarray(hold, MAX(carda, cardb));
+	hold = addition_r(hold, hold2, result);
+	printarray(hold, cardinal);
 	
-	/* subtract */
+	/* arb subtraction */
 	printf("subtraction\n");
 	hold = str2ints(argv[0], bigint1);
 	hold2 = str2ints(argv[1], bigint2);
-	hold = subtraction(hold, hold2, result);
-	printarray(hold, MAX(carda, cardb));
+	hold = subtraction_r(hold, hold2, result);
+	printarray(hold, cardinal);
 	
-	/* setarray, iszero, arb copyarray */
+	/* arb addition */
+	printf("simple addition\n");
+	hold = str2ints(argv[0], bigint1);
+	hold2 = str2ints(argv[1], bigint2);
+	addition(hold, hold2);
+	printarray(hold, cardinal);
+
+	/* arb subtract */
+	printf("simple subtraction\n");
+	hold = str2ints(argv[0], bigint1);
+	hold2 = str2ints(argv[1], bigint2);
+	subtract(hold, hold2);
+	printarray(hold, cardinal);
+	
+	/* arb setarray, arb iszero, arb copyarray */
 	printf("setarray. iszero, copyarray\n");
 	hold = str2ints(argv[0], bigint1);
 	hold2 = str2ints(argv[1], bigint2);
-	setarray(hold, 0, carda + cardb);
-	setarray(hold2, 1, carda + cardb);
-	printarray(hold, carda);
-	printarray(hold2, cardb);
+	setarray(hold, 0);
+	setarray(hold2, 1);
+	printarray(hold, cardinal);
+	printarray(hold2, cardinal);
 	if ( iszero(hold) == 0 )
 		printf("hold is zero\n");
 	if ( iszero(hold2) != 0 )
 		printf("hold2 is not zero\n");
-	copyarray(hold2, hold, carda + cardb);
+	copyarray(hold2, hold);
 	if ( iszero(hold2) == 0 )
 		printf("hold2 is now zero\n");
+
+	/* single digit arb multiplication */
+	printf("short multiplication\n");
+	hold = str2ints(argv[0], bigint1);
+	hold2 = str2ints(argv[1], bigint2);
+	short_multiply(hold, hold2[0]);
+	printarray(hold, cardinal);
+	
+	/* single digit arb short_short_divide */
+	printf("short division\n");
+	hold = str2ints(argv[0], bigint1);
+	hold2 = str2ints(argv[1], bigint2);
+	short_divide(hold, hold2[0]);
+	printarray(hold, cardinal);
 
 	return 0;
 } 
 
+#endif
